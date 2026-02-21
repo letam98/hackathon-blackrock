955fd3da2ae18ab6f552696d51f497330a378861
ketan kanoje <ketan.kanoje@mahindrafinance.com>
2026-02-21 07:26:55 +0530

    added first created project

---
+++ src/main/java/com/hackthon/hackathonapplication/HackathonapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.hackthon.hackathonapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackthon/hackathonapplication/HackathonapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackthon.hackathonapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
